01c1796b420bc18e908d6da97dccae49abeae3ac
refactor <>
2024-10-31 20:29:36 +0700

    init

---
+++ Pixabay/Pixabay/PixabayApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixabayApp.swift
+//  Pixabay
+//
+//  Created by Macbook on 31/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PixabayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
